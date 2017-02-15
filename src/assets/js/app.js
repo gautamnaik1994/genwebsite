f76664219b1f24cdbd0fbf4b055e149d49b1719f
gautamnaik1994 <gautamnaik1994@gmail.com>
2017-02-15 18:01:47 +0530

    fc

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,10 +2,26 @@ $(document).foundation();
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
-    autoplay:true,
+    autoplay: true,
     responsive: {
         0: {
             items: 1
         }
     }
-})
+});
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll > 300) {
+        $('.top-bar').removeClass('top-bar-before');
+        $('.top-bar').addClass('top-bar-after');
+        $("#logoImage").attr("src", "assets/img/logo_gen_h.svg");
+    } else {
+
+        $('.top-bar').addClass('top-bar-before');
+        $('.top-bar').removeClass('top-bar-after');
+                 $("#logoImage").attr("src", "assets/img/logo_gen.svg");
+    }
+
+});
