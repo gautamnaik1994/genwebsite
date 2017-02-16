09dc6478aba86f95230066e0a3d8acf9aa9d3a73
Gautam Naik <gautamnaik1994@gmail.com>
2017-02-16 07:23:15 +0530

    fc

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,8 @@
 $(document).foundation();
+var logoSquare = new Image()
+logoSquare.src = "assets/img/logo_gen_h.svg";
+var logoRect = new Image()
+logoRect.src = "assets/img/logo_gen.svg";
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
@@ -16,12 +20,13 @@ $(window).scroll(function (event) {
     if (scroll > 300) {
         $('.top-bar').removeClass('top-bar-before');
         $('.top-bar').addClass('top-bar-after');
-        $("#logoImage").attr("src", "assets/img/logo_gen_h.svg");
+        $("#logoImage").attr("src", logoSquare.src);
+        //document.getElementById("logoImage").src = logoSquare.src;
     } else {
 
         $('.top-bar').addClass('top-bar-before');
         $('.top-bar').removeClass('top-bar-after');
-                 $("#logoImage").attr("src", "assets/img/logo_gen.svg");
+                 $("#logoImage").attr("src", logoRect.src);
     }
 
 });
